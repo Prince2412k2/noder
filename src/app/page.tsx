66bac0af906ba13954f4c2ff9071745b8f6884ee
Prince <princempate.dev@gmail.com>
2025-11-15 12:34:24 +0530

    feat: theme and styling with shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   await requireAuth();
   const data = await caller.getUsers();
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
+    <div className="dark min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       <div>{JSON.stringify(data, null, 2)}</div>
       <LogoutButton />
     </div>
