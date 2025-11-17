1209ec37e02be281d2336586e71484ec4bdd5ef1
Prince <princempate.dev@gmail.com>
2025-11-17 12:27:07 +0530

    feat:added ai providers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { LogoutButton } from "./logout";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { useTRPC } from "@/trpc/client";
-import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
+import { useTRPC } from "@/trpc/client";
+import { LogoutButton } from "./logout";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   // await requireAuth();
@@ -12,14 +12,31 @@ export default function Home() {
 
   const { data } = useQuery(trpc.getWorkflows.queryOptions());
 
-  const create = useMutation(trpc.createWorkflow.mutationOptions({
-    onSuccess: () => { toast.success(("Job Queued")) }
-  }));
+  const testAi = useMutation(
+    trpc.testAi.mutationOptions({
+      onSuccess: () => {
+        toast.success("Ai job Queued");
+      },
+    }),
+  );
+
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        toast.success("Job Queued");
+      },
+    }),
+  );
 
   return (
     <div className="dark min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       <div>{JSON.stringify(data, null, 2)}</div>
-      <Button disabled={create.isPending} onClick={() => create.mutate()}>Create new workflow</Button>
+      <Button disabled={testAi.isPending} onClick={() => testAi.mutate()}>
+        Test Ai
+      </Button>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create new workflow
+      </Button>
       <LogoutButton />
     </div>
   );
