be3a6c718657fd3862161084e9a6ce50bfb78b63
Prince <prince@gmai.com>
2025-11-17 08:34:08 +0530

    WIP

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,17 @@
 import prisma from "@/lib/db";
 import { createTRPCRouter, protectedProcedure } from "../init";
+import { inngest } from "@/inngest/client";
 
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    return prisma.user.findMany({ where: { id: ctx.auth.user.id } });
+  // get workflows
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
   }),
+
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({ name: "test/hello.world", data: { email: "prince@2412.com", }, })
+    return { success: true, message: "Job queued" }
+  })
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
